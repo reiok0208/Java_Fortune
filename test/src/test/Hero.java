5b16284f0645a5ad8972c32d6875e5c091a916d7
reiok0208 <rei.okada.wii@gmail.com>
2020-07-11 13:48:24 +0900

    First Commit

---
+++ test/src/test/Hero.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Hero {
+  public static void main(String[] args) {
+	  for(int i = 0; i < 5; i++) {
+		  System.out.println("---");
+		  System.out.println(i);
+	  }
+  }
+}
